d9c8a21f50a2889e363ca3b7dc6f40a188b69861
Bruno Alves <vespidhook@gmail.com>
2023-01-20 14:32:54 -0300

    base front

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,17 @@
-import { Habit } from "./components/Habit";
-
 import './styles/global.css';
+import { Header } from './components/Header';
+import { SummaryTable } from './components/SummaryTable';
 
-function App() {
+// import { Habit } from "./components/Habit";
+
+export function App() {
   return (
-    <>
-      <Habit completed={3} />
-      <Habit completed={31} />
-      <Habit completed={32} />
-    </>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16"> 
+        <Header />
+        <SummaryTable />
+      </div>      
+    </div>
   )
 }
 
-export default App
